866e8a11e66f159e235f773fc4e52d061f9c7e31
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2022-11-09 01:12:53 +0100

    Add split_frames and extracting error fmessage rom ffmpeg error output

--- ffmpeg_util.py
+++ ffmpeg_util.py
@@ -1,3 +1,4 @@
+import re
 import json
 import subprocess
 from pathlib import Path
@@ -7,6 +8,14 @@ from util import TMP_PATHS
 from localization import localize_str
 
 _MAX_BUFFER_SIZE = 1024 * 1000 * 8  # 8Mb
+
+
+def ffmpeg_error_handler(stderr: str):
+    error_message = re.findall(
+        r'ffmpeg version [^\n]+\n(?:\s*built with [^\n]+\n|\s*lib[^\n]+\n|\s*configuration:[^\n]+\n)*([\s\S]*)',
+        stderr,
+    )[1]
+    print(error_message)
 
 
 def get_video_info(video_path: Path) -> Tuple[Tuple[int, int], str, int, int]:
@@ -67,3 +76,14 @@ def split_audio(video_path: Path) -> bool:
         return False
     return True
 
+
+def split_frames(video_path: Path, transparent: bool, threads: int):
+    command = ['ffmpeg', '-threads', f'{threads}', '-y']
+    if transparent:
+        command += ['-vcodec', 'libvpx']
+    tmp_frame_files = TMP_PATHS['tmp_frame_files']
+    command += [f'"{video_path}"', f'"{tmp_frame_files}"']
+    out = subprocess.run(command, bufsize=_MAX_BUFFER_SIZE, stderr=subprocess.PIPE, text=True)
+    if out.stderr:
+        ffmpeg_error_handler(out.stderr)
+        exit()
