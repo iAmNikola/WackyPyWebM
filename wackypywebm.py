5881879f807d682b750b28284e8b35c790cdc83b
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2022-11-09 01:14:19 +0100

    Rename variables to all-caps to inform that they have global scope when defined

--- wackypywebm.py
+++ wackypywebm.py
@@ -13,26 +13,26 @@ _SELECTED_MODES: List[str] = []
 
 
 if __name__ == '__main__':
-    args = PARSER.parse_args()
+    ARGS = PARSER.parse_args(['/home/wd-nikolad/work/scripts/out.mp4'])
 
-    if args.language:
-        set_locale(args.language)
+    if ARGS.language:
+        set_locale(ARGS.language)
 
-    if not args.file.is_file():
-        print(localize_str('video_file_not_found',  { 'file': str(args.file) }))
+    if not ARGS.file.is_file():
+        print(localize_str('video_file_not_found', {'file': str(ARGS.file)}))
         PARSER.print_help()
         exit()
+    ARGS.file = ARGS.file.resolve()
 
-    _SELECTED_MODES = [mode.lower() for mode in args.modes.split('+')]
-    for selected_mode in _SELECTED_MODES:
-        if selected_mode not in MODES:
-            print(f'Mode "{selected_mode}" isn\'t available.')
+    _SELECTED_MODES = [mode.lower() for mode in ARGS.modes.split("+")]
+    for SELECTED_MODE in _SELECTED_MODES:
+        if SELECTED_MODE not in MODES:
+            print(f'Mode "{SELECTED_MODE}" isn\'t available.')
             exit()
 
-    if args.output:
-        args.output = args.output.resolve()
-        args.output.parent.mkdir(parents=True, exist_ok=True)
+    if ARGS.output:
+        ARGS.output = ARGS.output.resolve()
+        ARGS.output.parent.mkdir(parents=True, exist_ok=True)
     else:
-        args.output = args.file.parent / f'{args.file.stem}_{"_".join(_SELECTED_MODES)}.webm'
-    
-    
+        ARGS.output = ARGS.file.parent / f'{ARGS.file.stem}_{"_".join(_SELECTED_MODES)}.webm'
+
