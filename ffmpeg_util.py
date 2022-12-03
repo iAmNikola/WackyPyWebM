ba8c0e8ea6507c80781800b06ca0dc322cb553ce
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2022-12-03 03:12:43 +0100

    Add function to execute ffmpeg commands. Removed double quotes to prevent failure

--- ffmpeg_util.py
+++ ffmpeg_util.py
@@ -1,11 +1,12 @@
-import re
 import json
+import re
 import subprocess
 from pathlib import Path
-from typing import Tuple
+from threading import Lock
+from typing import List, Tuple
 
-from util import TMP_PATHS
 from localization import localize_str
+from util import TMP_PATHS
 
 _MAX_BUFFER_SIZE = 1024 * 1000 * 8  # 8Mb
 
@@ -15,7 +16,7 @@ def ffmpeg_error_handler(stderr: str):
         r'ffmpeg version [^\n]+\n(?:\s*built with [^\n]+\n|\s*lib[^\n]+\n|\s*configuration:[^\n]+\n)*([\s\S]*)',
         stderr,
     )[1]
-    print(error_message)
+    print('FFMPEG ERROR:', error_message)
 
 
 def get_video_info(video_path: Path) -> Tuple[Tuple[int, int], str, int, int]:
@@ -36,8 +37,9 @@ def get_video_info(video_path: Path) -> Tuple[Tuple[int, int], str, int, int]:
         bufsize=_MAX_BUFFER_SIZE,
         capture_output=True,
         text=True,
+        encoding='utf-8',
     )
-    stream_data = json.loads(video_data.stdout)['streams']
+    stream_data = json.loads(video_data.stdout)['streams'][0]
     return (
         (stream_data['width'], stream_data['height']),
         stream_data['r_frame_rate'],
@@ -62,14 +64,15 @@ def split_audio(video_path: Path) -> bool:
                 'ffmpeg',
                 '-y',
                 '-i',
-                f'"{video_path}"',
+                f'{video_path}',
                 '-vn',
                 '-c:a',
                 'libvorbis',
-                f'"{tmp_audio}"',
+                f'{tmp_audio}',
             ],
             bufsize=_MAX_BUFFER_SIZE,
             check=True,
+            stderr=subprocess.PIPE,
         )
     except subprocess.CalledProcessError:
         print(localize_str('no_audio'))
@@ -82,8 +85,40 @@ def split_frames(video_path: Path, transparent: bool, threads: int):
     if transparent:
         command += ['-vcodec', 'libvpx']
     tmp_frame_files = TMP_PATHS['tmp_frame_files']
-    command += [f'"{video_path}"', f'"{tmp_frame_files}"']
-    out = subprocess.run(command, bufsize=_MAX_BUFFER_SIZE, stderr=subprocess.PIPE, text=True)
-    if out.stderr:
-        ffmpeg_error_handler(out.stderr)
+    command += ['-i', f'{video_path}', f'{tmp_frame_files}']
+    try:
+        subprocess.run(command, bufsize=_MAX_BUFFER_SIZE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
+    except subprocess.CalledProcessError as e:
+        ffmpeg_error_handler(e.stderr)
         exit()
+
+
+_LOCK = Lock()
+
+
+def execute_command(command: List[str], extra_data: Tuple[List[bool], int, int, int]):
+    try:
+        subprocess.run(command, bufsize=_MAX_BUFFER_SIZE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
+    except subprocess.CalledProcessError as e:
+        ffmpeg_error_handler(e.stderr)
+        return
+    if extra_data:
+        frames_processed, index, same_size_count, num_frames = extra_data
+        for i in range(index - same_size_count - 1, same_size_count - 1):
+            frames_processed[i] = True
+        _LOCK.acquire()
+        print('\r', end='')
+        print(
+            localize_str(
+                'convert_progress',
+                args={
+                    'framecount': f'{same_size_count:>5}',
+                    'startframe': index - same_size_count,
+                    'endframe': index,
+                    'batch_size': num_frames,
+                    'percent': f'{100*frames_processed.count(True) / num_frames:.1f}',
+                },
+            ),
+            end='',
+        )
+        _LOCK.release()
