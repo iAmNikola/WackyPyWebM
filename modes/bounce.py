c31f85aee081f7a289a85dc1f551c1068157d1da
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2022-12-03 03:09:07 +0100

    Return dictionary to support commands

--- modes/bounce.py
+++ modes/bounce.py
@@ -5,9 +5,9 @@ from interfaces import ModeBase, Info
 
 class Mode(ModeBase):
     @classmethod
-    def get_frame_bounds(info: Info):
+    def get_frame_bounds(cls, info: Info):
         if info.frame_index == 0:
-            return None, info.height
+            return {'height': info.height}
         else:
             height = math.floor(abs(math.cos((info.frame_index / (info.fps / info.tempo)) * math.pi) * info.height))
-            return None, height
+            return {'height': height}
